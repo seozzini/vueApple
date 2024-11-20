82e9f32b70c5fe9836b2d15393b66f31addc7b33
seojin <admin@DESKTOP-U820E4V>
2024-11-20 14:14:08 +0900

    20241120

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
+  <!-- <div v-if="1 == 2">안녕하세요</div>
+  <div v-else-if="1 == 3">안녕하세요2</div>
+  <div v-else>안녕하세요3</div> -->
 
-  <div class="black-bg" v-if="modalState == true">
-    <div class="white-bg">
-      <h4>상세페이지임</h4>
-      <p>상세페이지 내용임</p>
-      <button @click="modalState=false">닫기</button>
-    </div>
+  <ModalModal />
+
+  <div class="menu">
+    <a v-for="(a, i) in menus" :key="i">{{ a }}</a>
   </div>
 
-<div class="menu">
-  <a v-for="(a,i) in menus" :key="i">{{a}}</a>
-</div>
+  <DisCount />
 
-<!-- <div v-for="(b,j) in products" :key="j">
+  <!-- <div v-for="(b,j) in products" :key="j">
     <h4>{{ b }}</h4>
     <p>50 만원</p>
 </div> -->
-<div v-for="(작명,i) in onerooms" :key="i">
-  <img :src="onerooms[i].image" class="room-img">
-      <h4 @click="modalState = true">{{ onerooms[i].title }}</h4>
-      <p>{{onerooms[i].price}}원</p>
-      <button @click ="singo[i]++">허위매물신고</button><span> 신고 수 : {{ singo[i] }}</span>
-</div>
-
+  <div v-for="(a, i) in onerooms" :key="i">
+    <img :src="a.image" class="room-img" />
+    <h4
+      @click="
+        modalState = true;
+        누른거 = i;
+      "
+    >
+      {{ a.title }}
+    </h4>
+    <p>{{ a.price }}원</p>
+    <button @click="singo[i]++">허위매물신고</button
+    ><span> 신고 수 : {{ singo[i] }}</span>
+  </div>
 
   <!-- <div>
     <img :src="onerooms[0].image" class="room-img">
@@ -60,58 +66,67 @@
     <p>{{onerooms[5].price}}원</p>
     <button @click ="singo[5]++">허위매물신고</button><span> 신고 수 : {{ singo[5] }}</span>
   </div> -->
-
 </template>
 
 <script>
-import oneroomData from './assets/oneroom.js'
-oneroomData
+import oneroomData from './assets/oneroom.js';
+import DisCount from './components/DisCount.vue';
+import ModalModal from './components/ModalModal.vue';
+oneroomData;
 
 export default {
   name: 'App',
-  data(){
-    return{
+  data() {
+    return {
+      누른거: 0,
       onerooms: oneroomData,
-      modalState:false,
-      singo : [0,0,0,0,0,0],
-      menus : ['Home','Shop', 'About'],
-      products : ['역삼동원룸','천호동원룸','마포구원룸'],
-      prices:[50,60,70],
-      
-    }
+      modalState: false,
+      singo: [0, 0, 0, 0, 0, 0],
+      menus: ['Home', 'Shop', 'About'],
+      products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      prices: [50, 60, 70],
+    };
   },
-  methods:{
-    increase(){
+  methods: {
+    increase() {
       this.singo++;
-    }
-
+    },
   },
   components: {
-  }
-}
+    DisCount,
+    ModalModal,
+  },
+};
 </script>
 
 <style>
-body{
+body {
   margin: 0;
 }
-div{
+div {
   box-sizing: border-box;
 }
-.black-bg{
-  width: 100%; height:100px;
-  background: rgba(0,0,0,0.5);
-  position: fixed; 
+.discount {
+  background: #eee;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 5px;
+}
+.black-bg {
+  width: 100%;
+  height: 100px;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
   padding: 20px;
 }
-.white-bg{
+.white-bg {
   width: 100%;
   background: white;
   border-radius: 8px;
   padding: 20px;
 }
 
-.room-img{
+.room-img {
   width: 80%;
   margin-top: 40px;
 }
@@ -124,13 +139,12 @@ div{
   color: #2c3e50;
 }
 .menu {
-background: darkslateblue;
-padding: 15px;
-border-radius: 5px;
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
 }
 .menu a {
   color: white;
   padding: 10px;
 }
-
 </style>
---
+++ src/components/ModalModal.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="black-bg" v-if="modalState == true">
+    <div class="white-bg">
+      <img :src="onerooms[누른거].image" />
+      <h4>{{ onerooms[누른거].title }}</h4>
+      <p>{{ onerooms[누른거].content }}</p>
+      <p>가격: {{ onerooms[누른거].price }}원</p>
+      <DisCount />
+      <button @click="modalState = false">닫기</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ModalModal',
+};
+</script>
